96a9193b3409bcfbd8152e6ed2ec101dd352dc0a
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-04-12 16:43:50 +0900

    validation correction

--- app/models/training.rb
+++ app/models/training.rb
@@ -6,12 +6,12 @@ class Training < ApplicationRecord
   with_options presence: true do
     validates :name, length: { maximum: 50 }
     validates :place, length: { maximum: 100 }
-    validates :content, length: { maximum: 200 }
     validates :start_at
     validates :end_at
+    validates :content, length: { maximum: 200 }
   end
   validate :start_at_should_be_before_end_at
-
+  validates :prefecture_id, numericality: { other_than: 1 , message: 'を選択してください'}
   private
   def start_at_should_be_before_end_at
     return unless start_at && end_at
