e50a40723be2aa8bb34721b70c66187aa2c28657
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-04-14 18:01:55 +0900

    Setting front side

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,10 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
+
+require('comment-modal.js')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
