61524d865273375748fb43c4e055fb137b1e8039
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-04-17 18:44:27 +0900

    Setting top page front side

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
+
   def index
-    @trainings = Training.where('start_at > ?', Time.zone.now).order(:start_at)
+    @trainings = Training.page(params[:page]).per(10)
+                         .where('start_at > ?', Time.zone.now).order(:start_at)
   end
 end
