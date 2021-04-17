be7a1905d1b2f187f3f7e50ecc00b44a135ea132
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-04-17 14:13:55 +0900

    Setting model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:github]
+  before_destroy :check_all_events_finished
 
-  has_many :created_trainings, class_name: 'Training', foreign_key: 'owner_id'
-  has_many :tickets
+  has_many :created_trainings, class_name: 'Training', foreign_key: 'owner_id', dependent: :nullify
+  has_many :tickets, dependent: :nullify
+  has_many :participating_trainings, through: :tickets, source: :training
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)\w{6,12}\z/.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
@@ -21,5 +23,13 @@ class User < ApplicationRecord
       user.name = nickname
       user.image_url = image_url
     end
+  end
+
+  def check_all_events_finished
+    now = Time.zone.now
+    errors[:base] << '公開中の未終了イベントが存在します' if created_trainings.where(':now < end_at', now: now).exists?
+    errors[:base] << '未終了の参加イベントが存在します' if participating_trainings.where(':now < end_at', now: now).exists?
+    # エラー時は処理を中断
+    throw(:abort) unless errors.empty?
   end
 end
