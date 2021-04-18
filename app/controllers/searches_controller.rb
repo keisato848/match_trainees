1d6d4c08414d7785d1a09d4ec50387d393d722e4
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-04-18 20:54:14 +0900

    Setting controller and routing

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,17 @@
+class SearchesController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def index
+    t = Training.ransack(params[:q])
+    @results = t.result.page(params[:page]).per(12)
+                .where('start_at > ?', Time.zone.now).order(:start_at)
+    set_prefecture
+  end
+
+  private
+
+  def set_prefecture
+    q = params[:q]
+    @searched_prefecture = Prefecture.find(q[:prefecture_id_eq])
+  end
+end
