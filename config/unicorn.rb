f5fc1b648bb99dbbfff6230373c154f1e73f9b39
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-04-20 19:52:53 +0900

    Settings for automatic deployment

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,15 +1,27 @@
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
+
 worker_processes 1
-working_directory app_path
-pid "#{app_path}/tmp/pids/unicorn.pid"
-listen "#{app_path}/tmp/sockets/unicorn.sock"
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+working_directory "#{app_path}/current}"
+
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
 timeout 60
+
 preload_app true
+
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+
 check_client_connection false
+
 run_once = true
+
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) &&
     ActiveRecord::Base.connection.disconnect!
@@ -26,6 +38,7 @@ before_fork do |server, worker|
     end
   end
 end
+
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
