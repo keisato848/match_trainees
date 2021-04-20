a123ea350fc992cf7153f55ecac62e478e35cd40
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-04-20 20:12:59 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 lock '3.16.0'
 
-set :application, 'match-trainees'
+set :application, 'match_trainees'
 
-set :repo_url,  'git@github.com:keisato848/match-trainees.git'
+set :repo_url,  'git@github.com:keisato848/match_trainees.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
